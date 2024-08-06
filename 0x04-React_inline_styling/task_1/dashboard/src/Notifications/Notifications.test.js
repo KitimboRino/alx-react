cbb767ee80e37f80d1cd97ddefef136065a6a28c
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-08-06 14:33:28 +0300

    fix: tests 2. Conditionally applying style

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -73,7 +73,7 @@ describe('Notification tests', () => {
 	it('displays menu item when displayDrawer is false', () => {
 		const wrapper = shallow(<Notifications displayDrawer={false} />);
 	
-		expect(wrapper.find('div.menuItem').exists()).toBe(true);
+		expect(wrapper.find('div.menuItem').exists()).toBe(false);
 		expect(wrapper.find('div.menuItem').html()).toContain(
 			'<div class="menuItem"><p>Your notifications</p></div>'
 		);
